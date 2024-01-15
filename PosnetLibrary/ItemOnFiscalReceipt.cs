7b3cebda62268ebcbf16363fc50bfdda75a5735c
Jacek Kosiński <kashiash@gmail.com>
2024-01-15 22:14:28 +0100

    Add project files.

---
+++ PosnetLibrary/ItemOnFiscalReceipt.cs
@@ -0,0 +1,20 @@
+﻿namespace PosnetLibrary
+{
+    public class ItemOnFiscalReceipt
+    {
+        public ItemOnFiscalReceipt(string itemName, double grossAmount, double quantity, string vatSymbol, string? description = null )
+        {
+            ItemName = itemName;
+            GrossAmount = grossAmount;
+            Quantity = quantity;
+            VatRate = vatSymbol;
+            Description = description;
+        }
+        public string ItemName { get; set; }
+        public double GrossAmount { get; set; }
+
+        public double Quantity { get; set; }
+        public string VatRate { get; set; }
+        public string? Description { get; set; }
+    }
+}
